40824e4c9a2530ae9c92a372a9b56a122fcf7fcb
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-25 20:54:50 +0200

    db model and failing test #3

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  it "is valid with valid attributes" do
+    author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "'http://wikipedia.org/Alan_Turing'")
+    expect(author.first_name).to eq("Alan")
+    expect(author.last_name).to eq("Turing")
+    expect(author.homepage).to eq("'http://wikipedia.org/Alan_Turing'")
+    expect(author).to be_valid
+  end
+end
