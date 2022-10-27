1554762953c2ca9539636f2bdb9bb0c0dafb81bc
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-27 20:59:59 +0200

    run migration and add a model test

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    paper = Paper.new(title: "PaperTitle", venue: "Test", year: 2023)
+    expect(paper.title).to eq("PaperTitle")
+    expect(paper.venue).to eq("Test")
+    expect(paper.year).to eq(2023)
+    expect(paper).to be_valid
+  end
 end
