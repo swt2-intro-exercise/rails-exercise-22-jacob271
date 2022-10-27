714d40b2014078a1c5daa03153386452137252ee
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-27 20:14:32 +0200

    display error message after invalid submission for new author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,12 +3,16 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
   def new
+    @author = Author.new
   end
   def create
     @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
 
-    @author.save
-    redirect_to root_path, notice: 'Success!'
+    if @author.save
+      redirect_to root_path, notice: 'Success!'
+    else
+      render 'new'
+    end
   end
   def index
     @authors = Author.all
