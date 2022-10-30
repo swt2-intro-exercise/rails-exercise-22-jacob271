5add8fa2861fa0476ac5741e0ec0a2d2f15838a1
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-30 11:26:03 +0100

    create relation between authors and papers

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,5 @@ class Paper < ApplicationRecord
   validates :title, presence: true
   validates :year, numericality: { only_integer: true }
   validates :venue, presence: true
+  has_and_belongs_to_many :authors
 end
