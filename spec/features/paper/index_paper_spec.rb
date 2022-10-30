ff0ee4ee90bd302955c8c698177665294a87afe8
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-30 11:12:37 +0100

    failing test for link to edit paper page #15

---
+++ spec/features/paper/index_paper_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Index paper page", type: :feature do
+  it "should exist at 'paper_url' and render without error" do
+    # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
+    visit papers_url
+  end
+  it "should link to 'edit_paer_path'" do
+    @paper = FactoryBot.create :paper
+    visit papers_url
+    expect(page).to have_link 'Edit this paper', href: edit_paper_path(@paper)
+  end
+end
