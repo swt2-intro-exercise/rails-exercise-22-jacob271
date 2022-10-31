74d8a4b4b846d0a3173f7ca8b38222fcf67c2277
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-31 08:06:34 +0100

    add year scope to paper #20

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,5 @@ class Paper < ApplicationRecord
   validates :year, numericality: { only_integer: true }
   validates :venue, presence: true
   has_and_belongs_to_many :authors
-end
+  scope :year, ->(year) { where("year = ?", year) if year.present?}
+  end
